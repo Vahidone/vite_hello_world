2598a7c4063ae8a12d6c20c45f9f8b0b0676ff59
Vahidone <v.ban.roma@gmail.com>
2023-10-09 21:29:54 +0200

    feat: add component AppHero

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -8,13 +8,33 @@ export default {
 </script>
 
 <template>
-  <h2>jumbotron</h2>
+  <div class="container">
+    <h1>ciao jumbotron</h1>
+    <figure>
+      <img src="/163822766-57cf8569-bf06-42d3-be81-835ba3c10e09.jpg" alt="" />
+    </figure>
+  </div>
 </template>
 
 <style scoped>
-h2 {
-  color: palevioletred;
+figure {
+  width: 450px;
+  height: 400px;
+  margin: 20px auto;
+}
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.container {
+  background-color: lightblue;
+  height: 700px;
+  padding: 100px;
+}
+
+h1 {
+  color: rgb(65, 13, 13);
   text-align: center;
-  margin-top: 100px;
 }
 </style>
